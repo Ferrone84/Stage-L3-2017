69aa1c25fa06d60ea1edb8e70c22530bdc1534e8
DuretNicolasLaptop <nico.duret@orange.fr>
2017-01-19 16:25:20 +0100

    Commit des sources commentées et revues plus dynamiques des sources. Ajout d'exemple/ de fichier type pour les algorithmes.

--- src/InnToGraph.java
+++ src/InnToGraph.java
@@ -6,17 +6,42 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.File;
 
+/**
+ * Classe qui va parser les fichiers .ieq.poi (fichier Porta) contenant les facettes, en fichier _graph.txt qui va en extraire les sous graphes.<br>
+ * Cette classe aura besoin du fichier initial .txt qui contient le graphe, pour savoir si les liens entre les noeuds (des sous graphes) existent.<br>
+ * L'utilisateur pourras le fournir en 2éme argument si jamais le nom est différent.
+ */
 public class InnToGraph {
+
+	/**
+	* Nom du fichier .poi.ieq
+	*/
 	private String fileName;
+
+	/**
+	* Contiendra les sous graphes des innégalitées (une innégalité par ligne)
+	*/
 	private String[][] bonds;
+
+	/**
+	* Le nombre d'innégalité qu'a produit le fichier porta .poi.ieq
+	*/
 	private int dimension;
+
+	/**
+	* On créer un objet de l'autre classe pour pouvoir utiliser les méthodes qu'elle implémente qui lisent les fichiers .txt initiaux.
+	*/
 	private ParseurV2 parseur;
 
 	public InnToGraph(String fileName) {
 		this.fileName = fileName;
 	}
 
+	/**
+	* Méthode qui va afficher les sous graphes
+	*/
 	public void displayBonds() {
+		System.out.println("Facettes et leurs noeuds.");
 		for(int i=0; i < bonds.length; i++) {
 			for(int j=0; j < bonds[i].length; j++) {
 				System.out.print(bonds[i][j]+"|");
@@ -25,11 +50,14 @@ public class InnToGraph {
 		}
 	}
 
+	/**
+	* Méthode qui va lire le fichier pour savoir combien de facettes devront être lue.
+	*/
 	public void setDimInn() throws IOException{
 		BufferedReader file = null; //le fichier
 		String lineContent = ""; 	//le contenu de la ligne lue
 		int line = 0;				//le numero de la ligne
-		int tmp = 0;
+		int tmpDimension = 0;		//compteur qui contiendra le nombre de facettes (sans compter celles <= 0)
 
 		try{
 			file = new BufferedReader(new FileReader(this.fileName));
@@ -38,22 +66,30 @@ public class InnToGraph {
 			e.printStackTrace();
 		}
 
-		while((lineContent = file.readLine()) != null){
+		while((lineContent = file.readLine()) != null) { //on lis le fichier tant qu'on le peut
 			if(line > 5 && lineContent.compareTo("\n") != -1 && lineContent.compareTo("END") != 0
-				&& lineContent.charAt(lineContent.length()-1) != '0') {
-				tmp++;
+				&& lineContent.charAt(lineContent.length()-1) != '0') { 
+				//cette quadruple condition dit que :
+				// - on prend à partir de la ligne 6 (car les facettes sont toujours écrites à partir de cette ligne)
+				// - on ne prend pas les lignes vide
+				// - on ne prend pas le "END" à la fin
+				// - on ne prend pas les facettes <= 0 (car le charactère à la position "taille -1" est le chiffre à droite de l'innéquation)
+				tmpDimension++;
 			}
-			line++;
+			line++; //on va à la ligne suivante
 		}
 		file.close();
-		this.dimension = tmp;
+		this.dimension = tmpDimension; //on set la dimension avec le nombre de facettes lues
 	}
 
+	/**
+	* Cette méthode va lire le fichier .iop.ieq et le parser en récupérant les sous graphes
+	*/
 	public void read() throws IOException{
 		BufferedReader file = null; //le fichier
 		String lineContent = ""; 	//le contenu de la ligne lue
 		int line = 0;				//le numero de la ligne
-		int currentLine = 0;
+		int currentLine = 0;		//la facette courrante
 
 		try{
 			file = new BufferedReader(new FileReader(this.fileName));
@@ -61,21 +97,21 @@ public class InnToGraph {
 		catch(FileNotFoundException e){
 			e.printStackTrace();
 		}
-		bonds = new String[dimension][0];
+		bonds = new String[dimension][0]; //on réalloue le nombre de ligne grâce à la dimension
 
-		while((lineContent = file.readLine()) != null){
+		while((lineContent = file.readLine()) != null) { //on lis tant qu'on peut
 			if(line > 5 && lineContent.compareTo("\n") != -1 && lineContent.compareTo("END") != 0
-				&& lineContent.charAt(lineContent.length()-1) != '0') {
-				String[] tokens = lineContent.split("x");
-				for(int i=1; i < tokens.length; i++) {
-					tokens[i] = deletePlus(tokens[i]);
-					tokens[i] = deleteSpace(tokens[i]);
+				&& lineContent.charAt(lineContent.length()-1) != '0') { //même contraintes que dans setDimInn()
+				String[] tokens = lineContent.split("x"); //on split sur le charactère "x" pour récupérer chaque noeuds de chaque lignes du fichier
+				for(int i=1; i < tokens.length; i++) {	//cette boucle va permettre de parcourir tous les tokens obtenus
+					tokens[i] = deletePlus(tokens[i]);	//on supprime tous les "+" des tokens
+					tokens[i] = deleteSpace(tokens[i]);	//on supprime tous les " " des tokens
 				}
-				bonds[currentLine] = new String[tokens.length];
+				bonds[currentLine] = new String[tokens.length]; //on réalloue le nombre de colonne (nombre de noeuds)
 
-				this.bonds[currentLine][0] = lineContent;
+				this.bonds[currentLine][0] = lineContent;	//dans la case 0 de chaque colonne on aura l'innéquation correspondante écrite en brut
 				for(int i=1; i < tokens.length; i++) {
-					this.bonds[currentLine][i] = tokens[i];
+					this.bonds[currentLine][i] = tokens[i];	//on ajoute ensuite dans chaque colonne chaque noeuds lue
 				}
 				currentLine++;
 			}
@@ -84,19 +120,24 @@ public class InnToGraph {
 		file.close();
 	}
 
+	/**
+	* Cette méthode va écrire dans un nouveau fichier .txt pour afficher l'extraction des sous graphes des facettes.
+	*/
 	public void write() {
 		FileWriter dest = null;
 		String str = "";
-		String file = this.fileName.substring(0, fileName.length() - 8) + "2.txt";
+		String file = this.fileName.substring(0, fileName.length() - 8) + "_graph.txt"; //on créer un nouveau nom avec l'ancien
 		StringBuffer strbuff = new StringBuffer();
 
-		for(int i=0; i < this.bonds.length; i++) {
-			strbuff.append("INNEQUATION N°"+i+" : "+bonds[i][0] + "\n\t");
-			strbuff.append(this.bonds[i].length-1 + " : ");
-			for(int j=1; j < this.bonds[i].length-1; j++) {
-				for(int k = j+1; k < this.bonds[i].length; k++) {
-					if(itExist(Integer.parseInt(bonds[i][j]) -1, Integer.parseInt(bonds[i][k]) -1)) {
-						if(j != this.bonds[i].length-2)
+		for(int i=0; i < this.bonds.length; i++) { //on parcours chaque ligne
+			strbuff.append("INNEQUATION N°"+i+" : "+bonds[i][0] + "\n\t"); //on écrit l'innéquation pour la compréhension
+			strbuff.append(this.bonds[i].length-1 + " : "); //on met aussi le nombre de noeuds contenus dans le sous graphe
+
+			for(int j=1; j < this.bonds[i].length-1; j++) {	//on parcours à partir de la colonne 1 car en colonne 0 il y a la facette
+				for(int k = j+1; k < this.bonds[i].length; k++) { //cette boucle permet de tester tous les noeuds entre eux
+					if(itExist(Integer.parseInt(bonds[i][j]) -1, Integer.parseInt(bonds[i][k]) -1)) { //si le lien existe on l'ajoute dans le fichier
+
+						if(j != this.bonds[i].length-2) //condition qui évite d'avoir un " | " à la fin des lignes
 							strbuff.append(bonds[i][j]+"-"+bonds[i][k]+" | ");
 						else
 							strbuff.append(bonds[i][j]+"-"+bonds[i][k]);
@@ -122,14 +163,22 @@ public class InnToGraph {
 		}
 	}
 
+	/**
+	* Cette méthode va vérifier si dans la matrice de l'objet "parseur" il y a un lien entre les noeuds k et p.
+	*
+    * @param k
+    *			ligne k de la matrice
+    * @param p
+    *			colonne p de la matrice
+    * @return 	retourne true si le lien existe, false sinon
+	*/
 	public boolean itExist(int k, int p) {
-		if(this.parseur.getMatrixPos(k,p) == 1)
-			return true;
-
-		return false;
-
+		return (this.parseur.getMatrixPos(k,p) == 1);
 	}
 
+	/**
+	* Cette méthode va permettre de vérifier si le fichier initial est de type _bonds ou de type matrix.
+	*/
 	public void initParseur() {
 		BufferedReader file = null; //le fichier
 		String tmpName = this.fileName.substring(0, this.fileName.length() - 8) + "_bonds.txt";
@@ -137,22 +186,62 @@ public class InnToGraph {
 		File f = new File(tmpName);
 		if(f.exists() && !f.isDirectory()) { 
 			System.out.println("Fichier initial de type bonds.");
-			ParseurV2 parse = new ParseurV2(tmpName);
 		}
 		else {
 			System.out.println("Fichier initial de type matrix.");
 			tmpName = this.fileName.substring(0, this.fileName.length() - 8) + ".txt"; //si c'est pas un fichier bonds c'est un fichier matrix
-			
-			this.parseur = new ParseurV2(tmpName);
-			try {
-				this.parseur.parseMatrix();
-			}
-			catch(IOException e){
-				e.printStackTrace();
-			}
-		}		
+		}
+
+		this.parseur = new ParseurV2(tmpName);
+		try {
+			this.parseur.parseMatrix();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 
+	/**
+	* Même méthode que l'autre sauf que l'utilisateur précise le fichier initial en paramètre
+	*/
+	public void initParseur(String tmpName) {
+		BufferedReader file = null; //le fichier
+
+		File f = new File(tmpName);
+		if(f.exists() && !f.isDirectory()) {
+			this.parseur = new ParseurV2(tmpName);
+
+			if(tmpName.matches("(.*)_bonds(.*)")) {
+				try {
+					this.parseur.parseBonds();
+				}
+				catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+			else {
+				try {
+					this.parseur.parseMatrix();
+				}
+				catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+			System.out.println("Matrice correspondante.");
+			parseur.displayMatrix();
+		}
+		else {
+			System.out.println("Fichier initial introuvable.");
+		}
+	}
+
+	/**
+	* Cette méthode va permettre d'enlever chaque "+" de la chaine de charactère str.
+	*
+    * @param str
+    *			chaine de charactère à parser
+    * @return 	retourne le string une fois parsé
+	*/
 	private String deletePlus(String str) {
 		for(int i=0; i < str.length(); i++) {
 			if(str.charAt(i) == '+') {
@@ -162,6 +251,13 @@ public class InnToGraph {
 		return str;
 	}
 
+	/**
+	* Cette méthode va permettre d'enlever chaque " " de la chaine de charactère str.
+	*
+    * @param str
+    *			chaine de charactère à parser
+    * @return 	retourne le string une fois parsé
+	*/
 	private String deleteSpace(String str) {
 		for(int i=0; i < str.length(); i++) {
 			if(str.charAt(i) == ' ') {
@@ -177,18 +273,35 @@ public class InnToGraph {
 			System.out.println("Il faut rentrer le nom du fichier .poi.ieq en argument.");
 			return;
 		}
-		final String fileName = args[0];
-		InnToGraph object = new InnToGraph(fileName);
-		try {
-			object.initParseur();
-			object.setDimInn();
-			object.read();
-			object.displayBonds();
-			object.write();
+		else if(args.length == 1) { //s'il rentre juste le fichier .poi.ieq en argument
+			final String fileName = args[0];
+			InnToGraph object = new InnToGraph(fileName);
+			try {
+				object.initParseur();
+				object.setDimInn();
+				object.read();
+				object.displayBonds();
+				object.write();
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
 		}
-		catch(IOException e){
-			e.printStackTrace();
+		else if(args.length == 2) { //s'il rentre aussi le fichier initial (s'il a un nom différent)
+			final String fileName = args[0];
+			InnToGraph object = new InnToGraph(fileName);
+			try {
+				object.initParseur(args[1]);
+				object.setDimInn();
+				object.read();
+				object.displayBonds();
+				object.write();
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
 		}
+	
 	}
 }
 
--- src/ParseurV2.java
+++ src/ParseurV2.java
@@ -5,9 +5,27 @@ import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+/**
+ * Classe qui va permetre de parser des fichiers .txt contenant des graphes en forme de matrices ou en forme de liens.<br>
+ * Type matrice : première ligne on écrit la dimension, donc le nombre de sommets. Puis on met la matrice.<br>
+ * Type noeuds : le fichier devra s'écrire "nameFile" + "_bonds.txt".<br>
+ * Première ligne on écrit la dimension, donc le nombre de sommets. Et il devra contenir les liens entre les noeuds de la forme : 1-2 (un lien par ligne)
+ */
 public class ParseurV2 {
+
+	/**
+	* Nom du fichier à parser
+	*/
 	private String fileName;
+
+	/**
+	* Dimension = nombre de sommets du graphe
+	*/
 	private int dimension;
+
+	/**
+	* Matrice qui va contenir le graphe (0 => pas de lien / 1 => lien)
+	*/
 	private int[][] matrix;
 
 	public ParseurV2(String fileName) {
@@ -26,6 +44,23 @@ public class ParseurV2 {
 		this.dimension = dimension;
 	}
 
+	/**
+	 * Méthode qui va retourner la valeur de la case i,j
+     * 
+     * @param i
+     * 			ligne i de la matrice
+     * @param j
+     *			colonne j de la matrice
+     * @return 	la valeur de la case i,j
+     *
+     */
+	public int getMatrixPos(int i, int j) {
+		return matrix[i][j];
+	}
+
+	/**
+	 * Méthode qui va afficher la matrice
+     */
 	public void displayMatrix() {
 		for(int i=0; i < this.matrix.length; i++) {
 			for(int j=0; j < this.matrix[i].length; j++) {
@@ -35,6 +70,9 @@ public class ParseurV2 {
 		}
 	}
 
+	/**
+	 * Méthode qui va lire le fichier .txt et va en extraire une matrice qui représente le graphe 
+     */
 	public void parseMatrix() throws IOException{
 		BufferedReader file = null; //le fichier
 		String lineContent = ""; 	//le contenu de la ligne lue
@@ -46,20 +84,23 @@ public class ParseurV2 {
 		catch(FileNotFoundException e){
 			e.printStackTrace();
 		}
-		if((lineContent = file.readLine()) != null) {
-			this.dimension = Integer.parseInt(lineContent); //1ere ligne = nombre de ligne
+		if((lineContent = file.readLine()) != null) {			//on regarde la 1ere ligne
+			this.dimension = Integer.parseInt(lineContent); 	//1ere ligne = nombre de ligne
 		}
-		this.matrix = new int[this.dimension][this.dimension];
+		this.matrix = new int[this.dimension][this.dimension];	//on initialise la matrice de la classe
 
-		while((lineContent = file.readLine()) != null){
-			String[] tmp = lineContent.split(" ");
+		while((lineContent = file.readLine()) != null){			//on lis le fichier tant qu'on peut
+			String[] tmp = lineContent.split(" ");				//on coupe le string en tokens selon le délimiteur
 			for(int i=0; i < tmp.length; i++)
-				this.matrix[line][i] = Integer.parseInt(tmp[i]);
-			line++;
+				this.matrix[line][i] = Integer.parseInt(tmp[i]);//on récupère la valeur de chaque tokens
+			line++; //on passe à la ligne suivante
 		}
 		file.close();
 	}
 
+	/**
+	 * Méthode qui va faire la même chose que parseMatrix() mais qui va lire un fichier contenant les liens entre les noeuds
+     */
 	public void parseBonds() throws IOException{
 		BufferedReader file = null; //le fichier
 		String lineContent = ""; 	//le contenu de la ligne lue
@@ -86,17 +127,20 @@ public class ParseurV2 {
 				for(int i=0; i < this.matrix.length; i++)
 					for(int j=0; j < this.matrix[i].length; j++)
 						if(i == Integer.parseInt(tmp[k])-1 && j == Integer.parseInt(tmp[k+1])-1 
-						|| j == Integer.parseInt(tmp[k])-1 && i == Integer.parseInt(tmp[k+1])-1)
+						|| j == Integer.parseInt(tmp[k])-1 && i == Integer.parseInt(tmp[k+1])-1)	//la condition "ou" créer les doublons dans la matrice carré
 							matrix[i][j] = 1;
 		}
 		file.close();
 	}
 
+	/**
+	 * Méthode qui va écrire un fichier .ieq (fichier Porta) qui va correspondre au fichier .txt
+     */
 	public void write() {
-		FileWriter dest = null;
-		String str = "";
-		String fileIeq = getNameIeq();
-		StringBuffer strbuff = new StringBuffer();
+		FileWriter dest = null;	//objet fichier où l'on va écrire
+		String str = "";		//contiendra le string écrit dans le fichier
+		String fileIeq = getNameIeq(); //nom du fichier où l'on va écrire
+		StringBuffer strbuff = new StringBuffer();	//objet qui sera modifié pour contenir toute l'écriture
 
 		strbuff.append("DIM = "+this.dimension+"\n");
 		strbuff.append("VALID \n");
@@ -123,8 +167,8 @@ public class ParseurV2 {
 		try{
 			dest = new FileWriter(fileIeq);
 			BufferedWriter out = new BufferedWriter(dest);
-			str = strbuff.toString();
-			out.write(str);
+			str = strbuff.toString(); //on transforme en string
+			out.write(str); //et on l'écrit dans le fichier
 			out.flush();
 			out.close();
 		}
@@ -136,6 +180,11 @@ public class ParseurV2 {
 		}
 	}
 
+	/**
+	 * Méthode qui retourne le nom du fichier en .ieq
+	 *
+     * @return 	le string contenant le nom
+     */
 	public String getNameIeq() {
 		String str = "";
 		int length = 0;
@@ -160,20 +209,17 @@ public class ParseurV2 {
 			System.out.println("Sinon le fichier doit avoir '_bonds' dans son nom et il devra avoir les liens du graphe.");
 			return;
 		}
+		
 		final String fileName = args[0];
 		ParseurV2 parseur = new ParseurV2(fileName);
 		int length = 0;
 		boolean isMatrix = true; //fais la distinction entre un fichier matrice et un fichier lien
 
-		while(length < fileName.length()) {
-			if(fileName.charAt(length) == '_') {
-				isMatrix = false;
-				break;
-			}
-			length++;
-		}
+		if(fileName.matches("(.*)_bonds(.*)"))
+			isMatrix = false;
 
 		if(isMatrix) {
+			System.out.println("Fichier de type matrice.");
 			try{
 				parseur.parseMatrix();
 				parseur.displayMatrix();
@@ -184,6 +230,7 @@ public class ParseurV2 {
 			}
 		}
 		else {
+			System.out.println("Fichier de type bonds.");
 			try{
 				parseur.parseBonds();
 				parseur.displayMatrix();
