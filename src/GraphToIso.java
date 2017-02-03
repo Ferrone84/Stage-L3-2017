1c2768b68bc5c1fc279ef8123e86be2b8c125ac4
DuretNicolasLaptop <nico.duret@orange.fr>
2017-02-03 15:37:02 +0100

    Version commentée et presque terminée du parseur pour donner une matrice de similarité (version avant test).

--- src/GraphToIso.java
+++ src/GraphToIso.java
@@ -42,7 +42,7 @@ public class GraphToIso {
 	* Cette méthode va afficher la matrice de facettes.
 	*/
 	public void displayFacettes() {
-		System.out.println("Display facettes : (case 0 = nombre de sommets)");
+		System.out.println("Display facettes : (case 0 = nombre de sommets dans la facette)");
 		for(int i=0; i < facettes.length; i++) {
 			for(int j=0; j < facettes[i].length; j++) {
 				System.out.print(facettes[i][j]+"|");
@@ -56,7 +56,7 @@ public class GraphToIso {
 	* Cette méthode va initialiser la matrice de similarité grâce à la dimension.
 	*/
 	public void initSimilarityMatrix() {
-		similarityMatrix = new int[this.dimension][this.dimension];
+		this.similarityMatrix = new int[this.dimension][this.dimension];
 		for(int i=0; i < this.dimension; i++) {
 			for(int j=0; j < this.dimension; j++) {
 				this.similarityMatrix[i][j] = 0;
@@ -100,7 +100,7 @@ public class GraphToIso {
 			this.dimension = Integer.parseInt(lineContent); 	//1ere ligne = nombre de facettes
 		}
 
-		this.facettes = new String[dimension][0]; //on réalloue le nombre de ligne grâce à la dimension
+		this.facettes = new String[this.dimension][0]; //on réalloue le nombre de ligne grâce à la dimension
 
 		while((lineContent = file.readLine()) != null) { //on lis tant qu'on peut
 			if(line%2 == 1) { //permet de ne prendre qu'une ligne sur deux (celles qui nous intéressent)
@@ -125,45 +125,110 @@ public class GraphToIso {
 		file.close();
 	}
 
+	/**
+	* Méthode qui permet de savoir si les sous graphes sont isomorphes entre-eux. <br>
+	* S'ils le sont on met un 1 dans la matrice de similarité.
+	*
+	* @author Nicolas Duret &amp; Alexandra Moshina
+	*
+	*/
 	public void match() {
-		for(int i=0; i < facettes.length; i++) { //parcours les lignes
-			for(int k=i+1; k < facettes.length; k++) {
-				if(facettes[i][0].compareTo(facettes[k][0]) == 0 && facettes[i].length-1 == facettes[k].length-1) {
-					System.out.println("les facettes "+i+" et "+k+" ont le même nombre de sommets");
-					if(facettes[i].length-1 == 1) {
-						similarityMatrix[i][k] = 1;
-						similarityMatrix[k][i] = 1;
+		for(int i=0; i < this.facettes.length; i++) { //parcours les lignes
+			for(int k=i+1; k < this.facettes.length; k++) { //permet d'assossier les lignes i avec les lignes i+1 (évite les doublons)
+				//s'il y a le même nombre de sommets dans les facettes et qu'il y a le même nombre de liens (sinon pas isomorphe)
+				if(this.facettes[i][0].compareTo(this.facettes[k][0]) == 0 && this.facettes[i].length-1 == this.facettes[k].length-1) {
+					//System.out.println("Les facettes "+i+" et "+k+" ont le même nombre de sommets et le même nombre de liens.");
+					//s'il n'y a qu'un lien et 2 sommets alors les graphes sont forcément isomorphe (petite optimisation)
+					if(this.facettes[i][0].equals("2") && this.facettes[i].length-1 == 1) {
+						System.out.println("Les facettes "+i+" et "+k+" sont isomorphes.");
+						this.similarityMatrix[i][k] = 1;
+						this.similarityMatrix[k][i] = 1;
 					}
 					else {
-						String[] save = new String[ (facettes[i].length - 1) *4 ];
-						int nbsave = 0;
+						String save = ""; //va contenir les noeuds et leurs associations (avec des '=' pour les trier plus tard)
 
-						for(int j=1; j < facettes[i].length; j++) {
-							String[] nodesi = facettes[i][j].split("-");
-							String[] nodesk = facettes[k][j].split("-");
+						for(int j=1; j < this.facettes[i].length; j++) { //on va associer chaque noeud de i à un noeud de j correspondant
+							String[] nodesi = this.facettes[i][j].split("-"); //on récupère le lien i
+							String[] nodesk = this.facettes[k][j].split("-"); //on récupère le lien k
 
-							if(nbsave != 0) {
-								for(int p=0; p < nbsave; p+=2) {
-									if(save[p] != nodesi[0] && save[p] != nodesi[1]) {
-										
-									}
+							if ( !save.matches("(.*)" + nodesi[0] + "=(.*)") ) { //si on l'a pas déjà mis dans save on l'ajoute
+								save += nodesi[0] + "=" + nodesk[0] + "/";
+							}
+							if ( !save.matches("(.*)" + nodesi[1] + "=(.*)") ) { //la même pour le 2ème
+								save += nodesi[1] + "=" + nodesk[1] + "/";
+							}
+						}
+						save = save.substring(0, save.length()-1); //on enlève juste le '/' à la fin
+						//System.out.println(save);
+
+						String[] tokens = save.split("/"); //on split sur ce charactère
+						// for(int a=0; a < tokens.length; a++)
+						// 	System.out.print(tokens[a]+"|");
+						// System.out.println();
+
+						String[] tokensAndAsso = new String[(tokens.length * 2)]; //ce tableau va contenir chaque sommets, contenu dans tokens, et son association (sans les '=')
+						int z=0; //index pour se déplacer dans tokensAndAsso
+
+						for(int j=0; j < tokens.length; j++, z+=2) {
+							String[] tmp = tokens[j].split("=");
+							tokensAndAsso[z] = tmp[0]; //on met le premier
+							tokensAndAsso[z+1] = tmp[1]; //puis le 2ème
+						}
+
+						// for(int a=0; a < tokensAndAsso.length; a++)
+						// 	System.out.print(tokensAndAsso[a]+"|");
+						// System.out.println();
+
+						String[] substitut = new String[(this.facettes[i].length-1)]; //ce tableau va contenir la nouvelle chaine avec tous les liens remplacés par leurs substitutions
+						z = 0; //index pour se déplacer dans substitut
+
+						for(int j=1; j < this.facettes[i].length; j++) { //on parcours la chaine de i pour la transformer en celle de j (si la substitution est vraie)
+							//System.out.println(facettes[i][j]);
+							String[] nodes = this.facettes[i][j].split("-"); //on sépare le lien en deux
+
+							for(int b=0; b < tokensAndAsso.length; b+=2) { //on parcours tokensAndAsso sur les noeuds pairs (€ à i) car tokensAndAsso possède les noeuds et leurs subsituts
+								if(nodes[0].equals( tokensAndAsso[b] )) { //lorsque l'on a trouvé notre noeud
+									substitut[z] = tokensAndAsso[b+1]; //on ajoute son substitut
+									substitut[z] += "-"; //et on rajoute le petit lien
 								}
 							}
-							else { //1ere itération
-								save[nbsave++] = nodesi[0];
-								save[nbsave++] = nodesk[0];
-								save[nbsave++] = nodesi[1];
-								save[nbsave++] = nodesk[1];
+							for(int b=0; b < tokensAndAsso.length; b+=2) { //maintenant on cherche le 2éme noeud 
+								if(nodes[1].equals( tokensAndAsso[b] )) { //quand on l'a trouvé
+									substitut[z] += tokensAndAsso[b+1]; //on ajoute son substitut à la suite de son prédécesseur
+								}
 							}
+							z++; //on incrémente l'index de substitut
+						}
+
+						// System.out.print("Resultat de la substitution : ");
+						// for(int ab=0; ab < substitut.length; ab++) {
+						// 	System.out.print(substitut[ab]+"|");
+						// }
+						// System.out.println();
+
+						boolean isomorphe = true; //témoin si le graphe est isomorphe
+						z=0; //index qui va parcourir substitut
+						for(int ac = 1; ac < this.facettes[k].length; ac++, z++) { //on parcours enfin la 2éme facette pour vérifier si le substitut est pareil
+							if(! this.facettes[k][ac].equals(substitut[z])) //s'il y a une seule différence, les sous-graphes ne sont pas isomorphes
+								isomorphe = false;
+						}
+
+						//System.out.println(isomorphe);
+						if(isomorphe) {	//s'il le sont on met la matrice de similarité à jour
+							System.out.println("Les facettes "+i+" et "+k+" sont isomorphes.");
+							this.similarityMatrix[i][k] = 1;
+							this.similarityMatrix[k][i] = 1;
 						}
 					}
 				}
+				else
+					System.out.println("Les facettes "+i+" et "+k+" ne sont pas isomorphes.");
 			}
 		}
 	}
 
 	/**
-	* Cette méthode va écrire le contenue de la matrice de similarité dans un fichier _sim.txt .
+	* Cette méthode va écrire le contenu de la matrice de similarité dans un fichier _sim.txt .
 	*/
 	public void write() {
 		FileWriter dest = null;
@@ -171,7 +236,7 @@ public class GraphToIso {
 		String file = this.fileName.substring(0, fileName.length() - 10) + "_sim.txt"; //on créer un nouveau nom avec l'ancien
 		StringBuffer strbuff = new StringBuffer();
 
-		strbuff.append(dimension+"\n"); //nb facettes
+		strbuff.append(this.dimension+"\n"); //nb facettes
 		for(int i=0; i < this.similarityMatrix.length; i++) {
 			for(int j=0; j < this.similarityMatrix[i].length; j++) {
 				if(i == j)
@@ -213,6 +278,7 @@ public class GraphToIso {
 			object.initSimilarityMatrix();
 			object.match();
 			object.write();
+			System.out.println();
 			object.displaySimilarityMatrix();
 		}
 		catch(IOException e){
